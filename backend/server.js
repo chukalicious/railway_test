d47994da8d1643b0fa78914bcf1a043974805a2d
Katiuska Alicea <katiuskaalicea@gmail.com>
2024-06-01 11:38:06 -0700

    chore: Add MongoDB connection and update server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,20 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const port = process.env.PORT || 8000
-const cors = require("cors")
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+const cors = require('cors');
+const connectDB = require('./config/db');
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true })) 
-app.use(cors())
+connectDB();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-
-app.get("/", (req, res) => {
-    res.send({message: `Api is up 😊`} )
-}
-)   
-
-
-
-
-
+app.get('/', (req, res) => {
+  res.send({ message: `Api is up 😊` });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running in port ${port}`)
-    })  
+  console.log(`Server is running in port ${port}`);
+});
